e3e75dca3a936e64f257dc78ab2378d263f4c8a0
Kfir Nevo <nevo.kfir@gmail.com>
2019-07-06 15:10:01 +0300

    Delegation and Recording

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Kfir Nevo on 06/07/2019.
+//  Copyright © 2019 Kfir Nevo. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    var recordedAudioURL: URL!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
